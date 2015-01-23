94d293f2f0a84b81ade5aded073bced533a82b74
Nicole Deflaux <nicole.deflaux@gmail.com>
2015-01-23 14:37:10 -0800

    Removed some fields from the query results.
    To make the results easier to read.

--- pgp/sql/naive-variant-merge/end-only-used-for-non-sv.sql
+++ pgp/sql/naive-variant-merge/end-only-used-for-non-sv.sql
@@ -1,30 +1,12 @@
 # 'end' is not set when the record is not a reference-matching block or a
-# structural variant
+# structural variant.
 SELECT
   contig_name,
   start_pos,
   end_pos,
   reference_bases,
   GROUP_CONCAT(alternate_bases) WITHIN RECORD AS alt,
-  GROUP_CONCAT(STRING(AC)) WITHIN RECORD AS ac,
-  AN,
-  CGA_BF,
-  CGA_BNDG,
-  CGA_BNDGO,
-  GROUP_CONCAT(CGA_FI) WITHIN RECORD AS fi,
-  GROUP_CONCAT(CGA_MEDEL) WITHIN RECORD AS medel,
-  GROUP_CONCAT(CGA_MIRB) WITHIN RECORD AS mirb,
-  GROUP_CONCAT(CGA_PFAM) WITHIN RECORD AS pfam,
-  GROUP_CONCAT(CGA_RPT) WITHIN RECORD AS rpt,
-  CGA_SDO,
-  CGA_WINEND,
-  GROUP_CONCAT(CGA_XR) WITHIN RECORD AS xr,
-  GROUP_CONCAT(STRING(CIPOS)) WITHIN RECORD AS cipos,
   END,
-  IMPRECISE,
-  MATEID,
-  GROUP_CONCAT(MEINFO) WITHIN RECORD AS meinfo,
-  NS,
   SVLEN,
   SVTYPE
 FROM
--- pgp/sql/naive-variant-merge/sample-level-fields-after-merge.sql
+++ pgp/sql/naive-variant-merge/sample-level-fields-after-merge.sql
@@ -5,30 +5,15 @@ SELECT
   end_pos,
   reference_bases,
   alternate_bases,
-  CGA_BF,
-  CGA_BNDG,
-  CGA_BNDGO,
-  CGA_FI,
-  GROUP_CONCAT(CGA_MEDEL) WITHIN RECORD AS medel,
-  GROUP_CONCAT(CGA_MIRB) WITHIN RECORD AS mirb,
-  GROUP_CONCAT(CGA_PFAM) WITHIN RECORD AS pfam,
-  GROUP_CONCAT(CGA_RPT) WITHIN RECORD AS rpt,
-  CGA_SDO,
-  CGA_WINEND,
-  CGA_XR,
-  GROUP_CONCAT(STRING(CIPOS)) WITHIN RECORD AS cipos,
   END,
-  IMPRECISE,
-  MATEID,
-  GROUP_CONCAT(MEINFO) WITHIN RECORD AS meinfo,
   SVLEN,
   SVTYPE,
   call.callset_name AS callset_name,
-  GROUP_CONCAT(STRING(call.genotype)) WITHIN RECORD AS genotype,
+  GROUP_CONCAT(STRING(call.genotype)) WITHIN call AS genotype,
   call.phaseset AS phaseset,
-  GROUP_CONCAT(STRING(call.genotype_likelihood)) WITHIN RECORD AS genotype_likelihood,
+  GROUP_CONCAT(STRING(call.genotype_likelihood)) WITHIN call AS genotype_likelihood,
 FROM
-  FLATTEN([google.com:biggene:test.pgp_gvcf_variants_biallelic], call)
+  [google.com:biggene:test.pgp_gvcf_variants_biallelic]
 WHERE
   contig_name = '6'
   AND start_pos = 120458771
